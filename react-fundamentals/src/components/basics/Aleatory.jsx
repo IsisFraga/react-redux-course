5b2e3ff139cc2856c6ee70319c75a1af0885b1d2
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2021-06-16 15:00:24 -0300

    hotfix

--- react-fundamentals/src/components/basics/Aleatory.jsx
+++ react-fundamentals/src/components/basics/Aleatory.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function getRandom(min, max) {
-    var min = 0
-    var max = 50
-    var number = Math.floor(Math.random() * (max - min)) + min
+    const min = 0
+    const max = 50
+    const number = Math.floor(Math.random() * (max - min)) + min
     
     return (
         <div>
