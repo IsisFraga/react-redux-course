f67c7506b6408fec6d297d5c058407900c635c88
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2021-06-16 15:26:14 -0300

    creating cards

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -4,19 +4,32 @@ import First from './components/basics/First'
 import WithParam from './components/basics/WithParam'
 import Fragment from './components/basics/Fragment'
 import Aleatory from './components/basics/Aleatory'
+import Card from './components/layout/Card'
 
 export default() => {
     return (
         <div id="app">
             <h1>Fundamentos React (Arrow)</h1>
-            <Aleatory />
-            <Fragment />
-            <WithParam
-                titulo="Segundo Componente"
-                aluno="Pedro Silva"
-                nota={9.3} 
-            />
-            <First></First>
+
+            <Card title="#04 - Desafio Aleatório">
+                <Aleatory />
+            </Card>
+
+            <Card title="#03 - Fragmento">
+                <Fragment />
+            </Card>
+
+            <Card title="#02 - Com parâmetro">
+                <WithParam
+                    titulo="Segundo Componente"
+                    aluno="Pedro Silva"
+                    nota={9.3} 
+                />
+            </Card>
+
+            <Card title="#01 - Primeiro Componente">
+                <First />
+            </Card>
     </div>
     );
 }
